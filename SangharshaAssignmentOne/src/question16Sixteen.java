1d63c6184dea52b1f6fd42b41bfd2e9669bcceed
Sangharsha <sdhital4@mail.ccsf.edu>
2023-04-25 17:24:59 -0700

    Leap Year

--- SangharshaAssignmentOne/src/question16Sixteen.java
+++ SangharshaAssignmentOne/src/question16Sixteen.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 //16. Write a program to find the given number is even or odd.
-public class Question16Sixteen {
+public class question16Sixteen {
 
 	public static void main(String[] args) {
 		System.out.println("Enter the no.: ");
