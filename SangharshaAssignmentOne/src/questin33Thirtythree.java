c271d2c1a7c1986833ba6f55112c23cabd46fb00
Sangharsha <sdhital4@mail.ccsf.edu>
2023-05-13 12:52:14 -0700

    33 commit

--- SangharshaAssignmentOne/src/questin33Thirtythree.java
+++ SangharshaAssignmentOne/src/questin33Thirtythree.java
@@ -4,27 +4,27 @@ import java.util.Scanner;
 public class questin33Thirtythree {
 
 	public static void main(String[] args) {
-		Scanner input=new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the no. of integer you want to put in an Array: ");
-		int n=input.nextInt();
-		System.out.println("Ok! Enter "+n+" numbers:");
-		int[] a=new int[n];
-		int temp=0;
-		for(int i=0;i<n;i++) {
-			 a[i]=input.nextInt();
+		int n = input.nextInt();
+		System.out.println("Ok! Enter " + n + " numbers:");
+		int[] a = new int[n];
+		int temp = 0;
+		for (int i = 0; i < n; i++) {
+			a[i] = input.nextInt();
 		}
-		for(int i=0;i<n;i++) {
-			for(int j=i;j<n;j++) {
-			if(a[i]>a[j]) {
-				temp=a[i];
-				a[i]=a[j];
-				a[j]=temp;
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++) {
+				if (a[i] > a[j]) {
+					temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+
 			}
-		
 		}
-			}
 		System.out.println("The ascending order of the above arry entered is: ");
-		for(int each:a) {
+		for (int each : a) {
 			System.out.println(each);
 		}
 	}
