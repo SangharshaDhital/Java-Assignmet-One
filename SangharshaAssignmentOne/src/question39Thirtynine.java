6233ed94335b7faecbe569af38a923d484a0e830
Sangharsha <sdhital4@mail.ccsf.edu>
2023-05-15 22:22:28 -0700

    39th commit

---
+++ SangharshaAssignmentOne/src/question39Thirtynine.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//39. Write a Program to display names in short form. E.g. Ram Kishore Singh  -> R. k. Singh
+public class question39Thirtynine {
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter First name:");
+		String fName=input.next();
+		char f=fName.charAt(0);
+		System.out.println("Enter Middle name:");
+		String mName=input.next();
+		char m=mName.charAt(0);
+		System.out.println("Enter Last name:");
+		String lName=input.next();
+		System.out.println("The Short Verson of name: \n"+fName+" "+mName+" "+lName+" \nis :\n"+f+". "+m+". "+lName);
+		
+
+	}
+
+}
